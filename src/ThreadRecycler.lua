0cbc28ad4b2b1d8e7a2cc25dd9b519a246af1692
Illinois_Roadbuff <133445590+illinois-roadbuff@users.noreply.github.com>
2025-02-01 11:40:44 -0600

    Update ThreadRecycler.lua: Change variables (NOT TESTED)

--- src/ThreadRecycler.lua
+++ src/ThreadRecycler.lua
@@ -1,170 +1,182 @@
 --!strict
 --!native
--- Version 0.2.1-a.2
+-- Version 0.2.1-a.3
 --@illinois_roadbuff
 
-local t = {} -- t: Threads
-t.__index = t
+local Thread = {} -- Thread management
+Thread.__index = Thread
 
-type t = {
-	_c: boolean, -- _c: Callback? (Manual recycling?)
-	_t: thread, -- _t: Thread
+type Thread = {
+	useCallback: boolean, -- useCallback: Manual recycling?
+	coroutineThread: thread, -- coroutineThread: Thread
 }
 
-type r = { -- r: Recycling pool?
-	_t: number, -- Thread count
-	_o: {t}, -- _o: Open threads
-	_i: number, -- _i = Next index
-	_c: () -> nil, -- _c: Call
-	_n: (boolean) -> t, -- _n: Create thread
-	_y: () -> nil, -- _y: Yield
+type Recycler = { -- Recycler: Recycling pool
+	threadCount: number, -- threadCount: Thread count
+	availableThreads: {Thread}, -- availableThreads: Open threads
+	nextIndex: number, -- nextIndex: Next index
+	invokeCallback: () -> nil, -- invokeCallback: Call
+	createThread: (boolean) -> Thread, -- createThread: Create thread
+	yieldThread: () -> nil, -- yieldThread: Yield
 	spawn:<T...> (boolean, (T...) -> nil, T...) -> nil,
 	defer:<T...> (boolean, (T...) -> nil, T...) -> nil,
 	wrap:<T...> (boolean, (T...) -> nil, T...) -> nil,
-	delay:<T...> (boolean, number,  (T...) -> nil, T...) -> nil
+	delay:<T...> (boolean, number, (T...) -> nil, T...) -> nil
 }
 
-local r: r  = {} :: r
-setmetatable(r::r, t::t)
+local recycler: Recycler = {} :: Recycler
+setmetatable(recycler, Thread)
 
-r._t = 30 :: number --negative = dynamic
-r._o = {} :: {t}
+recycler.threadCount = 30 :: number -- negative = dynamic
+recycler.availableThreads = {} :: {Thread}
 
-local StartOnSignal:boolean = true :: boolean
-local i:number = 1 :: number
+local startWithSignal:boolean = true
+local currentIndex:number = 1
 local threadMetatable = {
 	__index = function(thread, key:string)
-		if key == "signal" then	return thread._c end
-		if key == "coroutine" then return thread._t end
+		if key == "signal" then return thread.useCallback end
+		if key == "coroutine" then return thread.coroutineThread end
 		return rawget(thread, key)
 	end
 }
 
-function t.recycle(index)
-	local thread:t = r._o[index]
-	r._o[i] = thread
-	i += 1
+function Thread.recycle(index)
+	local recycledThread:Thread = recycler.availableThreads[index]
+	recycler.availableThreads[currentIndex] = recycledThread
+	currentIndex += 1
 end
 
-function t._call<T...>(callback: (T...) -> nil, ...: T...)
-	local thread:t = r._o[ - 1]
-	if not thread then
-		t._createThread(false)  
-		thread = r._o[i - 1]
+function Thread._invoke<T...>(callback: (T...) -> nil, ...: T...)
+	local availableThread:Thread = recycler.availableThreads[-1]
+	if not availableThread then
+		Thread._createThread(false)  
+		availableThread = recycler.availableThreads[currentIndex - 1]
 	end
-	r._o[i - 1] = nil
-	i -= 1
-	if callback and typeof(callback) == "table" or typeof(callback) == "function" then
-		if thread._c then callback(...) else callback(...)
-			r._o[i] = thread
-			i += 1
+	recycler.availableThreads[currentIndex - 1] = nil
+	currentIndex -= 1
+
+	if callback and (typeof(callback) == "table" or typeof(callback) == "function") then
+		if availableThread.useCallback then
+			callback(...)
+		else
+			callback(...)
+			recycler.availableThreads[currentIndex] = availableThread
+			currentIndex += 1
 		end
-	else warn(`ThreadRecycler: Invalid callback recived: {typeof(callback)}. Is your callback a function?`) end
+	else
+		warn(`ThreadRecycler: Invalid callback received: {typeof(callback)}. Is your callback a function?`)
+	end
 end
 
-function t:_y(closeThread: boolean)
+function Thread:_yield(closeThread: boolean)
 	while not closeThread do
-		t._call(coroutine.yield())
+		Thread._invoke(coroutine.yield())
 	end
-	return
 end
 
-function t._createThread(UseSignal: boolean)
-	local newThread: t | nil
-	newThread = setmetatable({}, threadMetatable) :: t
-	newThread._c = UseSignal :: boolean
-	newThread._t = coroutine.create(r._y) :: thread
-	coroutine.resume(newThread._t :: thread, r :: r)
-	r._o[i] = newThread
-	i += 1
+function Thread._createThread(useSignal: boolean)
+	local newThread: Thread | nil
+	newThread = setmetatable({}, threadMetatable) :: Thread
+	newThread.useCallback = useSignal
+	newThread.coroutineThread = coroutine.create(recycler.yieldThread)
+	coroutine.resume(newThread.coroutineThread, recycler)
+	recycler.availableThreads[currentIndex] = newThread
+	currentIndex += 1
 end
 
-function t.spawn<T...>(UseSignal:boolean, callback: (T...) -> nil, ...: T...)
-	if #r._o < 1 then t._createThread(UseSignal) end
-	local thread:t = r._o[i - 1]
-	local index = i - 1::number
-	thread._c = UseSignal
-	local status:string = coroutine.status(thread._t)
-	if  status == "suspended" then
-		if not UseSignal then
-			task.spawn(thread._t::thread, callback:: (T...) -> nil, ...)
+function Thread.spawn<T...>(useSignal:boolean, callback: (T...) -> nil, ...: T...)
+	if #recycler.availableThreads < 1 then Thread._createThread(useSignal) end
+	local availableThread:Thread = recycler.availableThreads[currentIndex - 1]
+	local index = currentIndex - 1
+	availableThread.useCallback = useSignal
+	local status:string = coroutine.status(availableThread.coroutineThread)
+	if status == "suspended" then
+		if not useSignal then
+			task.spawn(availableThread.coroutineThread, callback, ...)
 		else
-			task.spawn(thread._t::thread, callback:: (T...) -> nil, index::number, ...)
+			task.spawn(availableThread.coroutineThread, callback, index, ...)
 		end
-	elseif  status == "dead" then
-		task.cancel(thread._t)
-		r._o[i - 1] = nil
-		i -= 1
-		t._createThread(UseSignal)
-		t.spawn(UseSignal, callback, ...) 
+	elseif status == "dead" then
+		task.cancel(availableThread.coroutineThread)
+		recycler.availableThreads[currentIndex - 1] = nil
+		currentIndex -= 1
+		Thread._createThread(useSignal)
+		Thread.spawn(useSignal, callback, ...)
 	end
 end
 
-function t.wrap<T...>(UseSignal:boolean, callback: (T...) -> nil, ...: T...)
-	if #r._o < 1 then t._createThread(UseSignal) end
-	local thread:t = r._o[i - 1]
-	local index = i - 1::number
-	thread._c = UseSignal
-	local status = coroutine.status(thread._t)
-	if  status == "suspended" then
-		if not UseSignal then
-			coroutine.resume(thread._t, callback, ...)
+function Thread.wrap<T...>(useSignal:boolean, callback: (T...) -> nil, ...: T...)
+	if #recycler.availableThreads < 1 then Thread._createThread(useSignal) end
+	local availableThread:Thread = recycler.availableThreads[currentIndex - 1]
+	local index = currentIndex - 1
+	availableThread.useCallback = useSignal
+	local status:string = coroutine.status(availableThread.coroutineThread)
+	if status == "suspended" then
+		if not useSignal then
+			coroutine.resume(availableThread.coroutineThread, callback, ...)
 		else
-			coroutine.resume(thread._t, callback, index, ...) 
+			coroutine.resume(availableThread.coroutineThread, callback, index, ...)
 		end
-	elseif  status == "dead" then
-		r._o[i - 1] = nil
-		i -= 1
-		t._createThread(UseSignal)
-		t.wrap(UseSignal, callback, ...) 
+	elseif status == "dead" then
+		task.cancel(availableThread.coroutineThread)
+		recycler.availableThreads[currentIndex - 1] = nil
+		currentIndex -= 1
+		Thread._createThread(useSignal)
+		Thread.spawn(useSignal, callback, ...)
 	end
 end
 
-function t.defer<T...>(UseSignal:boolean, callback: (T...) -> nil, ...: T...)
-	if #r._o < 1 then t._createThread(UseSignal) end
-	local thread:t = r._o[i - 1]
-	local index = i - 1::number
-	thread._c = UseSignal
-	local status = coroutine.status(thread._t)
-	if  status == "suspended" then
-		if not UseSignal then
+
+function Thread.defer<T...>(useSignal:boolean, callback: (T...) -> nil, ...: T...)
+	if #recycler.availableThreads < 1 then Thread._createThread(useSignal) end
+	local availableThread:Thread = recycler.availableThreads[currentIndex - 1]
+	local index = currentIndex - 1
+	availableThread.useCallback = useSignal
+	local status:string = coroutine.status(availableThread.coroutineThread)
+	if status == "suspended" then
+		if not useSignal then
 			task.wait()
-			coroutine.resume(thread._t, callback, ...)
+			coroutine.resume(availableThread.coroutineThread, callback, ...)
 		else
 			task.wait()
-			coroutine.resume(thread._t, callback, index, ...)
+			coroutine.resume(availableThread.coroutineThread, callback, index, ...)
 		end
-	elseif  status == "dead" then
-		r._o[i - 1] = nil
-		i -= 1
-		t._createThread(UseSignal)
-		t.defer(UseSignal, callback, ...)
+	elseif status == "dead" then
+		task.cancel(availableThread.coroutineThread)
+		recycler.availableThreads[currentIndex - 1] = nil
+		currentIndex -= 1
+		Thread._createThread(useSignal)
+		Thread.spawn(useSignal, callback, ...)
 	end
 end
 
-function t.delay<T...>(UseSignal, time:number, callback: (T...) -> nil, ...: T...)
-	if #r._o < 1 then t._createThread(UseSignal) end
-	local thread:t = r._o[i - 1]
-	local index = i - 1::number
-	thread._c = UseSignal
-	local status = coroutine.status(thread._t)
-	if  status == "suspended" then
-		if not UseSignal then
+function Thread.delay<T...>(useSignal:boolean, time:number, callback: (T...) -> nil, ...: T...)
+	if #recycler.availableThreads < 1 then Thread._createThread(useSignal) end
+	local availableThread:Thread = recycler.availableThreads[currentIndex - 1]
+	local index = currentIndex - 1
+	availableThread.useCallback = useSignal
+	local status:string = coroutine.status(availableThread.coroutineThread)
+	if status == "suspended" then
+		if not useSignal then
 			task.wait(time)
-			coroutine.resume(thread._t, callback, ...)
+			coroutine.resume(availableThread.coroutineThread, callback, ...)
 		else
 			task.wait(time)
-			coroutine.resume(thread._t, callback, ...)
+			coroutine.resume(availableThread.coroutineThread, callback, index, ...)
 		end
-	elseif  status == "dead" then
-		r._o[i - 1] = nil
-		i -= 1
-		t._createThread(UseSignal)
-		t.delay(UseSignal, t, callback, ...)
+	elseif status == "dead" then
+		task.cancel(availableThread.coroutineThread)
+		recycler.availableThreads[currentIndex - 1] = nil
+		currentIndex -= 1
+		Thread._createThread(useSignal)
+		Thread.spawn(useSignal, callback, ...)
 	end
 end
 
-for n = 1, math.abs(r._t), 1 do
-	t._createThread(StartOnSignal)
+for n = 1, math.abs(recycler.threadCount), 1 do
+	Thread._createThread(startWithSignal)
 end
-return t
+return Thread
+
+
+
