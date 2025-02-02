ef1a6d9ebb73317bab1539030bf287341919c291
Illinois_Roadbuff <133445590+illinois-roadbuff@users.noreply.github.com>
2025-02-02 11:32:06 -0600

    Update next.config.js

--- docs/next.config.js
+++ docs/next.config.js
@@ -1,6 +1,8 @@
-const withNextra = require('nextra')({
-  theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.tsx',
-})
+import nextra from "nextra";
 
-module.exports = withNextra()
+const withNextra = nextra({
+  theme: "nextra-theme-docs",
+  themeConfig: "./theme.config.tsx",
+});
+
+export default withNextra();
