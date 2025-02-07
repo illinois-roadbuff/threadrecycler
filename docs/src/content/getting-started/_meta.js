51d94febd38c82ce2e00e76908c7283b85cae230
Illinois_Roadbuff <133445590+illinois-roadbuff@users.noreply.github.com>
2025-02-06 18:48:56 -0600

    Update _meta.js

--- docs/src/content/getting-started/_meta.js
+++ docs/src/content/getting-started/_meta.js
@@ -1,5 +1,5 @@
 export default {
   '1-installation': 'Installation',
-  '3-config': 'Configuration',
-  '4-best': 'Example & Best Practices',
+  '2-config': 'Configuration',
+  '3-best': 'Example & Best Practices',
 }
